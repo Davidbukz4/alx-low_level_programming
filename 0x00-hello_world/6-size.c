ee7795063651024797fbc7b21d47f31620fbec2d
David Egwuatu <davidegwuatu2@gmail.com>
2022-09-08 17:46:57 -0700

    Task 6 added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char myChar;
+	int myInt;
+	long myLong;
+	long long myLongLong;
+	float myFloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(myLong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(myLongLong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
+	return (0);
+}
