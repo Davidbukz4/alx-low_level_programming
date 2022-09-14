4494ec34d899d2ae2c98c5f64b2953022889ef26
David Egwuatu <davidegwuatu2@gmail.com>
2022-09-13 17:25:18 -0700

    Task added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,21 +14,21 @@ void times_table(void)
 
 	for (x = 0; x < 10; x++)
 	{
+		_putchar(48);
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
+			_putchar(44);
+			_putchar(32);
 			if (z < 10)
 			{
-			_putchar(z + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar(32);
+			_putchar(z + 48);
 			}
 			else
 			{
-			_putchar((z / 10) + '0');
-			_putchar((z % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar((z / 10) + 48);
+			_putchar((z % 10) + 48);
 			}
 		}
 		_putchar('\n');
