a97b64b79e223e2484f4376f87089bae1b1560f8
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-01 14:57:09 -0700

    Task added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+
 /**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * main - adds positive integers
+ * @argc: number of argument
+ * @argv: pointer to array of argument
+ * Return: 0
  */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
-}
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
+
 int main(int argc, char *argv[])
 {
-	int result, i;
+	unsigned int x, result, temp;
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+	if (argc < 1)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%d\n", 0);
+		return;
 	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
+	while (argc-- && argc > 0)
+	{
+		for (x = 0; argv[argc][x] != '\0'; x++)
+		{
+			if (!(isdigit(argv[argc][x])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		temp = atoi(argv[argc]);
+		result = result + temp;
+	}
 	printf("%d\n", result);
-	return (0);
+	return (result);
 }
