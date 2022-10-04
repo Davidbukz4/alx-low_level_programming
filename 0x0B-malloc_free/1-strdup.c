c5e906e2adbf26a16150a25575d78fa8b8aa250d
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-04 08:43:37 -0700

    Task added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,27 +10,24 @@
 
 char *_strdup(char *str)
 {
-	char *ptr;
-	unsigned int len, x;
+	char *p;
+	unsigned int len, j;
 
 	if (str == NULL)
 		return (NULL);
 	for (len = 0; str[len] != '\0'; len++)
 		;
-
+	len++;
 	if (len < 1)
 		return (NULL);
-
-	ptr = malloc(len * sizeof(char));
-	if (ptr == NULL)
+	p = malloc(len * sizeof(char));
+	if (p == NULL)
 	{
-		free(ptr);
+		free(p);
 		return (NULL);
 	}
-
-	for (x = 0; x < len; x++)
-		ptr[x] = str[x];
-	ptr[x] = '\0';
-
-	return (ptr);
+	for (j = 0; j < len; j++)
+		p[j] = str[j];
+	p[j] = '\0';
+	return (p);
 }
