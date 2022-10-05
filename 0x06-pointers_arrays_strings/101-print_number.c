ab2bd965ddc2cf1bf68006a798029de82adc2b3e
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-04 19:25:07 -0700

    Print number added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,38 +1,23 @@
 #include "main.h"
 
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+
 void print_number(int n)
 {
-	int lenofExpo, number, i, temp, expo;
+	unsigned int number = n;
 
-	number = n;
-	expo = lenofExpo = 1;
 
 	if (number < 0)
 	{
-		number *= -1; // convert negative it to a positive integer so as to perform other operation
-		_putchar('-'); // put the minus sign to show that it's a negative number
+		number = -number;
+		_putchar('-');
 	}
 
-	/* counts length of exponent */
-	temp = number;
-	while (temp >= 10)
-	{
-		lenofExpo++; // length of expo (i.e 1 corresponds to 10, 2 corresponds to 100 etc.)
-		temp /= 10; // divides the number by 10 
-	}
-
-	/* creates exponent for the number */
-	for (i = 1; i < lenofExpo; i++)
-	{
-		expo *= 10;
-	}
-
-	/* print all the digits except the last one */
-	while (expo > 1)
-	{
-		_putchar((number / expo) % 10 + '0'); // print a single digit from the number starting from the first digit
-		expo /= 10; // reduce the exponent so as to get the next digit
-	}
+	if ((number / 10) > 0)
+		print_number(number / 10);
 
 	_putchar((number % 10) + '0');
 }
