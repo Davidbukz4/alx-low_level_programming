27e1a5b9885a0ad381191e2084c68f478b229a90
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-10 13:20:36 -0700

    Task added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,8 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+#endif
