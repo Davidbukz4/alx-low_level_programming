6ce37cb3c16962d3ce844461d2ccc370a30c2133
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-13 16:45:23 -0700

    Hot fix on task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,8 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(str, char *));
-
+		word = va_arg(str, char *);
 		word ? printf("%s", word) : printf("(nil)");
 
 		if (separator != NULL && i < (n - 1))
