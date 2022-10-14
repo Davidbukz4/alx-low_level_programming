f801daa2cb9c6412b5658dfaedff7ca0a79a90a7
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-13 19:07:18 -0700

    Task 3 added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,7 +14,6 @@ typedef struct valid_data_types
 	void (*func)();
 } data_type;
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
