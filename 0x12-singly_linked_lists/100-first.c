4285100167cbba59b4e86a0ab9e95683c80475fb
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-20 22:44:18 -0700

    Task added

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * pre_main - executes before main function
  */
 
-void pre_main(void) __attribute__((constructor))
+void __attribute__((constructor)) pre_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my ");
 	printf("back!\n");
