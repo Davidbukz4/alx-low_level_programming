3266509918fe79851280f3463cb24079030b0455
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-24 18:36:47 -0700

    Task 9 fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,15 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 
-	while (temp->next != NULL)
-	{
-		count++;
-		temp = temp->next;
-	}
-
-	if (idx > count)
-		return (NULL);
-
 	temp = *head;
 	while (i < idx - 1)
 	{
