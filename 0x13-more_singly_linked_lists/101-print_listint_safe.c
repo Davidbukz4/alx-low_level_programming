bb7efd7582b8cf681ecb1aecc395b38506acb947
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-25 11:15:23 -0700

    Task 12 added

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to first node
+ * Return: the number of node in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int count;
+	long int addr_diff;
+
+	count = 0;
+	if (!head)
+		exit(98);
+	while (head)
+	{
+		addr_diff = head - head->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		if (addr_diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			count++;
+			break;
+		}
+	}
+	return (count);
+}
