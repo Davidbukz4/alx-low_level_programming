a4b76724a1b3f1feaae1f36be16540cc236cbc34
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-27 16:36:23 -0700

    Task added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * power - returns the value of x raised to the power of y
- * @x: base number
- * @y: power of
- * Return: x to the power of y
- */
-
-int power(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	return (x * power(x, y - 1));
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 characters
  * Return: the converted characters
@@ -24,23 +8,17 @@ int power(int x, int y)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, sum, digit;
+	unsigned int i, num;
 
-	len = sum = digit = 0;
-	if (b == NULL)
+	num = 0;
+	if (!b)
 		return (0);
-	for (i = 0; b[i]; i++)
-		len++;
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		if (b[i] == '0')
-			digit = 0;
-		else
-			digit = 1;
-		sum += (digit * power(2, len - 1));
-		len--;
+		num = num << 1;
+		num = num | (b[i] - '0');
 	}
-	return (sum);
+	return (num);
 }
