fd64721318ba413f1b96cc9b8e92e3ad4dc62e48
David Egwuatu <davidegwuatu2@gmail.com>
2022-11-01 04:48:10 +0100

    Task 1 fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
 
 	for (f_len = 0; text_content[f_len]; f_len++)
 		;
