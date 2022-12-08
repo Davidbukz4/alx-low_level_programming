012f3f644e7319e6fec47f9b9e996d9f024003f4
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-08 20:21:31 +0100

    fixed betty error on task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the number of element in a linked dlistint_t list
+ * @h: pointer to the first node
+ * Return: number of element in the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	unsigned int i = 0;
